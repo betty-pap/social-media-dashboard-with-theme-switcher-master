bc46c02a8fde6d432d8111e7f9d45349f000bfdd
Beata Pap <beti_pap@yahoo.com>
2025-04-07 16:17:25 +0300

    deploying on github

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
 import react from '@vitejs/plugin-react';
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  base: "/social-media-dashboard-with-theme-switcher-master/"
 });
