761a66ca0b61fff3869a9088e2f5ec699281cbcc
Beata Pap <beti_pap@yahoo.com>
2025-04-09 13:49:23 +0300

    4th try

--- data.js
+++ data.js
@@ -1,19 +1,19 @@
 const statistics = [
-  {username: '@nathanf', number: 1987, followers: 'Followers', statToday: '12 Today', statColor: 'green', logo: 'images/icon-facebook.svg', pattern: 'fb'},
-  {username: '@nathanf', number: 1044, followers: 'Followers', statToday: '99 Today', statColor: 'green', logo: "images/icon-twitter.svg", pattern: 'tw'},
-  {username: '@realnathanf', number: '11k', followers: 'Followers', statToday: '1099 Today', statColor: 'green', logo: 'images/icon-instagram.svg', pattern: "ig"},
-  {username: 'Nathan F.', number: 8239, followers: 'Subscribers', statToday: '144 Today', statColor: 'red', logo: 'images/icon-youtube.svg', pattern: 'yt'},
+  {username: '@nathanf', number: 1987, followers: 'Followers', statToday: '12 Today', statColor: 'green', logo: '/blob/main/images/icon-facebook.svg', pattern: 'fb'},
+  {username: '@nathanf', number: 1044, followers: 'Followers', statToday: '99 Today', statColor: 'green', logo: "/blob/main/images/icon-twitter.svg", pattern: 'tw'},
+  {username: '@realnathanf', number: '11k', followers: 'Followers', statToday: '1099 Today', statColor: 'green', logo: '/blob/main/images/icon-instagram.svg', pattern: "ig"},
+  {username: 'Nathan F.', number: 8239, followers: 'Subscribers', statToday: '144 Today', statColor: 'red', logo: '/blob/main/images/icon-youtube.svg', pattern: 'yt'},
 ];
 
 const overviewData = [
-  { title: "Page Views", number: 87, change: "3%", changeColor: "green", logo: 'images/icon-facebook.svg'},
-  { title: "Likes", number: 52, change: "2%", changeColor: "red", logo: 'images/icon-facebook.svg'},
-  { title: "Likes", number: 5462, change: "2257%", changeColor: "green", logo: 'images/icon-instagram.svg'},
-  { title: "Profile Views", number: "52k", change: "1375%", changeColor: "green", logo: 'images/icon-instagram.svg'},
-  { title: "Retweets", number: 117, change: "303%", changeColor: "green", logo: "images/icon-twitter.svg"},
-  { title: "Likes", number: 507, change: "553%", changeColor: "green", logo: "images/icon-twitter.svg"},
-  { title: "Likes", number: 107, change: "19%", changeColor: "red", logo: 'images/icon-youtube.svg'},
-  { title: "Total Views", number: 1407, change: "12%", changeColor: "red", logo: 'images/icon-youtube.svg'},
+  { title: "Page Views", number: 87, change: "3%", changeColor: "green", logo: '/blob/main/images/icon-facebook.svg'},
+  { title: "Likes", number: 52, change: "2%", changeColor: "red", logo: '/blob/main/images/icon-facebook.svg'},
+  { title: "Likes", number: 5462, change: "2257%", changeColor: "green", logo: '/blob/main/images/icon-instagram.svg'},
+  { title: "Profile Views", number: "52k", change: "1375%", changeColor: "green", logo: '/blob/main/images/icon-instagram.svg'},
+  { title: "Retweets", number: 117, change: "303%", changeColor: "green", logo: "/blob/main/images/icon-twitter.svg"},
+  { title: "Likes", number: 507, change: "553%", changeColor: "green", logo: "/blob/main/images/icon-twitter.svg"},
+  { title: "Likes", number: 107, change: "19%", changeColor: "red", logo: '/blob/main/images/icon-youtube.svg'},
+  { title: "Total Views", number: 1407, change: "12%", changeColor: "red", logo: '/blob/main/images/icon-youtube.svg'},
 ];
 
 export  {statistics, overviewData};
