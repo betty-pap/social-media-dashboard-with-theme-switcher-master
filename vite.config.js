5dd97c55c4032a335488f106dd9b26129a5e780b
Betty Pap <130661020+betty-pap@users.noreply.github.com>
2025-04-22 14:02:35 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,5 @@ import { defineConfig } from "vite";
 import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
-  base: "/social-media-dashboard-with-theme-switcher-master/"
+  base: process.env.VITE_BASE_PATH || "/social-media-dashboard-with-theme-switcher-master/"
 });
